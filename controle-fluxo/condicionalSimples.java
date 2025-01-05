c2dd0ee59c6d40113e851f9a70bcf0fb6011ed7f
Raphazelll <pimentaraphael000@gmail.com>
2025-01-05 18:16:19 -0300

    primeiro commit? organizando essa IDE

--- controle-fluxo/condicionalSimples.java
+++ controle-fluxo/condicionalSimples.java
@@ -1,5 +1,17 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+public class condicionalSimples {
+
+/* Estrutura condicional
+        A estrutura condicional possibilita a escolha de um grupo de ações e comportamentos a serem
+    executadas quando determinadas. Condições são ou não satifeitas. A estrutura condicional pode ser
+    Simples ou Composta.
+
+    Condicional Simples
+        Ocorre quando uma validação de execução de fluxo somente quando a condição for positiva,
+    consideramos como estrutura SIMPLES.
+  
+*/
+
 }
+
+   
+
